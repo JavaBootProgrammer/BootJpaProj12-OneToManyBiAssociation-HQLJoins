7bab3f697d3588e5d07954dce612b409c990c84c
apasunoori <anand8533@gmail.com>
2024-06-23 19:26:09 -0400

    1

---
+++ src/main/java/com/nt/BootJpaProj12_OneToManyBiAssociation_HQLJoins.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj12_OneToManyBiAssociation_HQLJoins {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj12_OneToManyBiAssociation_HQLJoins.class, args);
+	}
+
+}
